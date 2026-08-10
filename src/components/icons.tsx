d9f73b1c5142de16c7361dfd2c5fe4126ffd3acf
premhagargi <prem@bloortech.com>
2026-08-10 13:12:13 +0530

    style: reduce thickness of 'i' and 'n' letters in LinkedIn icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,24 +7,15 @@ export const Icons = {
   email: (props: IconProps) => <MailIcon {...props} />,
   linkedin: (props: IconProps) => (
     <svg
-      viewBox="0 0 20 20"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 24 24"
       fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <title>linkedin</title>
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g transform="translate(-180.000000, -7479.000000)" fill="currentColor">
-          <g transform="translate(56.000000, 160.000000)">
-            <path
-              d="M144,7339 L140,7339 L140,7332.001 C140,7330.081 139.153,7329.01 137.634,7329.01 C135.981,7329.01 135,7330.126 135,7332.001 L135,7339 L131,7339 L131,7326 L135,7326 L135,7327.462 C135,7327.462 136.255,7325.26 139.083,7325.26 C141.912,7325.26 144,7326.986 144,7330.558 L144,7339 L144,7339 Z M126.442,7323.921 C125.093,7323.921 124,7322.819 124,7321.46 C124,7320.102 125.093,7319 126.442,7319 C127.79,7319 128.883,7320.102 128.883,7321.46 C128.884,7322.819 127.79,7323.921 126.442,7323.921 L126.442,7323.921 Z M124,7339 L129,7339 L129,7326 L124,7326 L124,7339 Z"
-              id="linkedin-[#161]"
-            />
-          </g>
-        </g>
-      </g>
+      <path d="M5 8h3v11H5z" />
+      <path d="M6.5 6.75a1.75 1.75 0 1 0 0-3.5 1.75 1.75 0 0 0 0 3.5z" />
+      <path d="M10 8h3v1.765c1.396-2.586 7-2.777 7 2.476V19h-3v-5.604c0-3.368-4-3.113-4 0V19h-3V8z" />
     </svg>
   ),
   x: (props: IconProps) => (
